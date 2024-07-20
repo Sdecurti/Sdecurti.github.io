72473cb93ae78d8888589471bb12a18c1b788e9b
Donovan Palmer <donovan@donovanpalmer.net>
2024-07-20 13:25:54 -0700

    updating

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        'sRed': '#ef333f',
+        'sYellow': '#fdf950',
+        'sGray': '#5c6473',
+        'sblue': '#497891',
+      }
+    }
   }
 }
