b3da9ff0788f84c694521fc42cbb1e3c6310bcbb
Donovan Palmer <donovan@donovanpalmer.net>
2025-01-28 15:54:46 -0800

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,21 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        'sRed': '#ef333f',
-        'sYellow': '#fdf950',
-        'sGray': '#5c6473',
-        'sBlue': '#497891',
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px'
       },
-      borderRadius: {
-        '6xl': '6rem'
-      }
+    },
+    colors: {
+      'sRed': '#ef333f',
+      'sYellow': '#fdf950',
+      'sGray': '#5c6473',
+      'sBlue': '#497891',
+    },
+    borderRadius: {
+      '6xl': '6rem'
     }
   }
+}
 }
